c6f01eac9b1376ca57455c3709f91ae4b7225ed6
keinner david <foreverkeinner@gmail.com>
2024-10-09 12:34:39 -0400

    Loggin

---
+++ src/libs/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+const globalForPrisma = globalThis;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
