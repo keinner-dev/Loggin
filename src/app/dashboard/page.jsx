48b34cbfa62ab73fb83f68b36b162638779bf469
keinner david <foreverkeinner@gmail.com>
2024-10-10 12:58:04 -0400

    boton de logout

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,18 +1,41 @@
 "use client"
-import {signOut} from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from "next/navigation";
+
 
 function DashboardPage() {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/auth/login")
+    }
+  })
+
+  if (status === "loading") {
+    return "Loading..."
+  }
+
+  function handleLogout() {
+    signOut({ callbackUrl: '/auth/login' })
+  }
+
   return (
     <section className="h-[calc(100vh-7rem)] flex justify-center items-center">
       <div>
         <h1 className="text-white text-5xl">Dashboard</h1>
         <button className="bg-white text-black px-4 py-2 rounded-md mt-4"
-          onClick={() => signOut()}
+          onClick={() => handleLogout()}
         >
           Logout
         </button>
+
+
+
       </div>
     </section>
   )
 }
+
+
 export default DashboardPage
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,18 @@
+import Link from "next/link"
+
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className="w-full">
+      <h1 className="text-white text-8xl font-bold text-center mt-12">Demo Next Auth</h1>
+
+      <div className="flex justify-center mt-6 gap-2">
+        <Link href={'/auth/login'} className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</Link>
+        <Link href={'/auth/register'} className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        Register
+        </Link>
+        
+      </div>
+    </div>
   )
 }
 
